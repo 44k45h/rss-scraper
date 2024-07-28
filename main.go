6ce516d1389ef6869ccab85eaef524f4117bb340
44k45h <bagde.akash@outlook.co>
2024-07-28 12:10:50 +0000

    added many to many relation and feed DELETE operation

--- main.go
+++ main.go
@@ -58,6 +58,10 @@ func main() {
 	v1Router.Post("/feeds", apiCfg.middlerwareAuth(apiCfg.handlerCreateFeed))
 	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
 
+	v1Router.Post("/feed_follows", apiCfg.middlerwareAuth(apiCfg.handlerCreateFeedFollow))
+	v1Router.Get("/feed_follows", apiCfg.middlerwareAuth(apiCfg.handlerGetFeedFollow))
+	v1Router.Delete("/feed_follows/{feedFollowID}", apiCfg.middlerwareAuth(apiCfg.handlerDeleteFeedFollow))
+
 	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
